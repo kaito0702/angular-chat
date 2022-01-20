6ea14ef9a848b4c6828fa7516876117014ecb9f3
kaito0702 <kaito.kitagawa@gmo-media.jp>
2022-01-20 23:44:17 +0900

    ヘッダーコンポーネントの作成

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,14 +6,16 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { environment } from '../../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
+  exports: [HeaderComponent],
 })
 export class CoreModule {}
