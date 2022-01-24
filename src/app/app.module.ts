c3dd4d8e822b56617efbe7846e12312cb4672220
kaito0702 <kaito.kitagawa@gmo-media.jp>
2022-01-24 19:25:21 +0900

    user一覧表示させる

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChatComponent } from './chat/chat.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +9,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatComponent, SignUpComponent, LoginComponent],
+  declarations: [AppComponent, SignUpComponent, LoginComponent],
   imports: [BrowserModule, SharedModule, CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/timeline/user-list/user-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { collection, Firestore } from '@angular/fire/firestore';
+import { collectionData } from 'rxfire/firestore';
+import { Observable } from 'rxjs';
+import { User, userConverter } from 'src/app/class/user';
+
+@Component({
+  selector: 'ac-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent implements OnInit {
+  users$: Observable<User[]>;
+
+  constructor(private firestore: Firestore) {
+    const users = collection(firestore, 'users').withConverter(userConverter);
+
+    this.users$ = collectionData(
+      collection(firestore, 'users').withConverter(userConverter)
+    );
+  }
+  ngOnInit(): void {}
+}
